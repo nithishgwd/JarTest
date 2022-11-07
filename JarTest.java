546c50055b07af69ef1aa4892392f5c5026e8177
Nithish M <89405692+nithishgwd@users.noreply.github.com>
2022-11-07 13:34:43 +0530

    Add files via upload

---
+++ JarTest.java
@@ -0,0 +1,9 @@
+package nithish_221047018;
+
+import jar.Student;
+public class JarTest {
+	public static void main(String[] args) {
+		Student s = new Student(221047018,"Nithish","Cloud Computing");
+	}
+	
+}
